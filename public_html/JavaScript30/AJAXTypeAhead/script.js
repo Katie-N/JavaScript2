f8d043381a47633533c47bb87e5e38f807bbb870
Katie-N <kaitlyn.nordberg@gmail.com>
2021-09-13 16:27:52 -0500

    AJAX type ahead project

---
+++ public_html/JavaScript30/AJAXTypeAhead/script.js
@@ -0,0 +1,6 @@
+// Katie Nordberg 9-13-2021
+
+// const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const endpoint = 'http://students.jccc.edu/knordber/javascript30/ajaxtypeahead/petOwnerStats.json';
+
+fetch(endpoint).then(console.log(data));
